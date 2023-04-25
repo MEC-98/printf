6224fa04bb15e6e8c8e72f863be068340aba15f2
Mostafa El Chahmouti <126358648+MEC-98@users.noreply.github.com>
2023-04-25 23:11:19 +0000

    1

--- main.c
+++ main.c
@@ -38,7 +38,7 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%l, %l]\n");
-    printf("Unknown:[%l, %l]\n");
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
     return (0);
 }
